04d76eba76f4259a137b161d52e5335382729023
ridargustia <ridargp@gmail.com>
2022-06-08 11:48:26 +0700

    Membuat tampilan halaman form pembuatan surat keterangan domisili

---
+++ application/controllers/Sk_domisili.php
@@ -0,0 +1,113 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sk_domisili extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->data['module'] = 'Surat Keterangan Domisili';
+
+        $this->data['company_data']      = $this->Company_model->company_profile();
+        $this->data['footer']            = $this->Footer_model->footer();
+
+        $this->data['btn_submit'] = 'Submit';
+        $this->data['btn_reset']  = 'Reset';
+        $this->data['btn_add']    = 'Tambah Data';
+    }
+
+    function create()
+    {
+        $this->data['page_title'] = 'Surat Keterangan Domisili';
+
+        $this->data['name'] = [
+            'name'          => 'name',
+            'id'            => 'name',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['nik'] = [
+            'name'          => 'nik',
+            'id'            => 'nik',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthplace'] = [
+            'name'          => 'birthplace',
+            'id'            => 'birthplace',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthdate'] = [
+            'name'          => 'birthdate',
+            'id'            => 'birthdate',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['gender'] = [
+            'name'          => 'gender',
+            'id'            => 'gender',
+            'class'         => 'form-control',
+        ];
+        $this->data['gender_value'] = [
+            '0'             => '-',
+            '1'             => 'Laki-laki',
+            '2'             => 'Perempuan',
+        ];
+        $this->data['status'] = [
+            'name'          => 'status',
+            'id'            => 'status',
+            'class'         => 'form-control',
+        ];
+        $this->data['status_value'] = [
+            '0'             => '-',
+            '1'             => 'Belum Kawin',
+            '2'             => 'Kawin',
+            '3'             => 'Cerai Hidup',
+            '4'             => 'Cerai Mati',
+        ];
+        $this->data['agama'] = [
+            'name'          => 'agama',
+            'id'            => 'agama',
+            'class'         => 'form-control',
+        ];
+        $this->data['agama_value'] = [
+            '0'             => '-',
+            '1'             => 'Islam',
+            '2'             => 'Kristen Protestan',
+            '3'             => 'Kristen Katolik',
+            '4'             => 'Buddha',
+            '5'             => 'Hindu',
+            '6'             => 'Konghucu',
+        ];
+        $this->data['kebangsaan'] = [
+            'name'          => 'kebangsaan',
+            'id'            => 'kebangsaan',
+            'class'         => 'form-control',
+        ];
+        $this->data['kebangsaan_value'] = [
+            '0'             => '-',
+            '1'             => 'WNI',
+            '2'             => 'WNA',
+        ];
+        $this->data['pekerjaan'] = [
+            'name'          => 'pekerjaan',
+            'id'            => 'pekerjaan',
+            'class'         => 'form-control',
+        ];
+        $this->data['address'] = [
+            'name'          => 'address',
+            'id'            => 'address',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'rows'           => '2',
+        ];
+
+        $this->load->view('front/surat/create_sk_domisili', $this->data);
+    }
+}
