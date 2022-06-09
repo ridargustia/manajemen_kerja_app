30540563c4a2e158ebdaf8fb7809d8fa6893a534
ridargustia <ridargp@gmail.com>
2022-06-09 22:42:07 +0700

    Membuat tampilan halaman form pembuatan surat keterangan usaha

---
+++ application/controllers/Sk_usaha.php
@@ -0,0 +1,103 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Sk_usaha extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->data['module'] = 'Surat Keterangan Usaha';
+
+        $this->data['company_data']      = $this->Company_model->company_profile();
+        $this->data['footer']            = $this->Footer_model->footer();
+
+        $this->data['btn_submit'] = 'Submit';
+        $this->data['btn_reset']  = 'Reset';
+        $this->data['btn_add']    = 'Tambah Data';
+    }
+
+    function create()
+    {
+        $this->data['page_title'] = 'Surat Keterangan Usaha';
+
+        $this->data['name'] = [
+            'name'          => 'name',
+            'id'            => 'name',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['nik'] = [
+            'name'          => 'nik',
+            'id'            => 'nik',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthplace'] = [
+            'name'          => 'birthplace',
+            'id'            => 'birthplace',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthdate'] = [
+            'name'          => 'birthdate',
+            'id'            => 'birthdate',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['gender'] = [
+            'name'          => 'gender',
+            'id'            => 'gender',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['gender_value'] = [
+            '0'             => '',
+            '1'             => 'Laki-laki',
+            '2'             => 'Perempuan',
+        ];
+        $this->data['agama'] = [
+            'name'          => 'agama',
+            'id'            => 'agama',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['agama_value'] = [
+            '0'             => '',
+            '1'             => 'Islam',
+            '2'             => 'Kristen Protestan',
+            '3'             => 'Kristen Katolik',
+            '4'             => 'Buddha',
+            '5'             => 'Hindu',
+            '6'             => 'Konghucu',
+        ];
+        $this->data['address'] = [
+            'name'          => 'address',
+            'id'            => 'address',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'rows'          => '2',
+            'required'      => '',
+        ];
+        $this->data['usaha_name'] = [
+            'name'          => 'usaha_name',
+            'id'            => 'usaha_name',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['address_usaha'] = [
+            'name'          => 'address_usaha',
+            'id'            => 'address_usaha',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+
+        $this->load->view('front/surat/create_sk_usaha', $this->data);
+    }
+}
