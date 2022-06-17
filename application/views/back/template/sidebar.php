82ec057f099eb3bebdf10738d2cb8b445009b122
ridargustia <ridargp@gmail.com>
2022-06-17 22:39:20 +0700

    Membuat fitur get data sk usaha untuk ditampilkan pada halaman admin (back)

--- application/views/back/template/sidebar.php
+++ application/views/back/template/sidebar.php
@@ -114,6 +114,13 @@
                   if ($count > 0) {
                   ?>
                     <span class="badge" style="margin-top: -9px; font-size:11px; padding: 3px 6px; background-color:firebrick;"><?php echo $count ?></span>
+                  <?php
+                  }
+                } elseif ($m->menu_controller === 'sk_usaha') {
+                  $count = $this->Sk_usaha_model->total_rows_is_not_readed();
+                  if ($count > 0) {
+                  ?>
+                    <span class="badge" style="margin-top: -9px; font-size:11px; padding: 3px 6px; background-color:firebrick;"><?php echo $count ?></span>
                 <?php
                   }
                 }
