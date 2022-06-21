0c19a0f224657c446095dae2e8a7d879d6f7a9e8
ridargustia <ridargp@gmail.com>
2022-06-21 21:49:30 +0700

    Membuat sweetalert untuk notifikasi sukses input data sk pindah (front)

--- application/views/front/surat/create_sk_pindah.php
+++ application/views/front/surat/create_sk_pindah.php
@@ -14,9 +14,7 @@
 
                 <!-- Main content -->
                 <section class="content">
-                    <?php if ($this->session->flashdata('message')) {
-                        echo $this->session->flashdata('message');
-                    } ?>
+                    <div class="flash-data" data-flashdata="<?php echo $this->session->flashdata('message') ?>"></div>
                     <?php echo form_open_multipart($action, array('id' => 'add_form')) ?>
                     <div class="row">
                         <div class="col-lg-12">
@@ -174,19 +172,19 @@
                                     <div class="row after-add-more">
                                         <!-- <div class="col-md-4">
                                             <div class="form-group">
-                                                <?php //echo form_input($nik_pengikut) 
+                                                <?php //echo form_input($nik_pengikut)
                                                 ?>
                                             </div>
                                         </div>
                                         <div class="col-md-4">
                                             <div class="form-group">
-                                                <?php //echo form_input($pengikut_name) 
+                                                <?php //echo form_input($pengikut_name)
                                                 ?>
                                             </div>
                                         </div>
                                         <div class="col-md-3">
                                             <div class="form-group">
-                                                <?php //echo form_input($keterangan) 
+                                                <?php //echo form_input($keterangan)
                                                 ?>
                                             </div>
                                         </div> -->
