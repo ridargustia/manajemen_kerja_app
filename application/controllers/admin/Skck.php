a6fd0d7573eee5bfcdfd83d69b88729b43057091
ridargustia <ridargp@gmail.com>
2022-06-23 21:27:42 +0700

    Membuat tampilan form untuk tambah data skck (back)

--- application/controllers/admin/Skck.php
+++ application/controllers/admin/Skck.php
@@ -42,4 +42,93 @@ class Skck extends CI_Controller
 
         $this->load->view('back/skck/skck_list', $this->data);
     }
+
+    function create()
+    {
+        //TODO Inisialisasi variabel
+        $this->data['page_title'] = 'Tambah Data ' . $this->data['module'];
+        $this->data['action']     = 'admin/skck/create_action';
+
+        //TODO Get data untuk dropdown reference
+        $this->data['get_all_combobox_status'] = $this->Status_model->get_all_combobox();
+        $this->data['get_all_combobox_agama'] = $this->Agama_model->get_all_combobox();
+        $this->data['get_all_combobox_pekerjaan'] = $this->Pekerjaan_model->get_all_combobox();
+        $this->data['get_all_combobox_pendidikan_akhir'] = $this->Pendidikan_akhir_model->get_all_combobox();
+
+        //TODO Rancangan form
+        $this->data['name'] = [
+            'name'          => 'name',
+            'id'            => 'name',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['nik'] = [
+            'name'          => 'nik',
+            'id'            => 'nik',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthplace'] = [
+            'name'          => 'birthplace',
+            'id'            => 'birthplace',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthdate'] = [
+            'name'          => 'birthdate',
+            'id'            => 'birthdate',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['gender'] = [
+            'name'          => 'gender',
+            'id'            => 'gender',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['gender_value'] = [
+            '0'             => '- Pilih Jenis Kelamin -',
+            '1'             => 'Laki-laki',
+            '2'             => 'Perempuan',
+        ];
+        $this->data['status'] = [
+            'name'          => 'status',
+            'id'            => 'status',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['agama'] = [
+            'name'          => 'agama',
+            'id'            => 'agama',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['pekerjaan'] = [
+            'name'          => 'pekerjaan',
+            'id'            => 'pekerjaan',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['pendidikan_akhir'] = [
+            'name'          => 'pendidikan_akhir',
+            'id'            => 'pendidikan_akhir',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['address'] = [
+            'name'          => 'address',
+            'id'            => 'address',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'rows'          => '2',
+            'required'      => '',
+        ];
+
+        //TODO Load view dengan mengirim data
+        $this->load->view('back/skck/skck_add', $this->data);
+    }
 }
