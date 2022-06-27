078e7af167942cd13870cbae9dd342bf4df35385
ridargustia <ridargp@gmail.com>
2022-06-27 16:23:53 +0700

    Membuat fitur tambah data sk usaha pada sistem admin (back)

--- application/controllers/admin/Sk_usaha.php
+++ application/controllers/admin/Sk_usaha.php
@@ -40,4 +40,132 @@ class Sk_usaha extends CI_Controller
 
         $this->load->view('back/sk_usaha/sk_usaha_list', $this->data);
     }
+
+    function create()
+    {
+        //TODO Inisialisasi variabel
+        $this->data['page_title'] = $this->data['module'];
+        $this->data['action']     = 'admin/sk_usaha/create_action';
+
+        //TODO Get data untuk dropdown reference
+        $this->data['get_all_combobox_agama'] = $this->Agama_model->get_all_combobox();
+
+        //TODO Rancangan form
+        $this->data['name'] = [
+            'name'          => 'name',
+            'id'            => 'name',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['nik'] = [
+            'name'          => 'nik',
+            'id'            => 'nik',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthplace'] = [
+            'name'          => 'birthplace',
+            'id'            => 'birthplace',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthdate'] = [
+            'name'          => 'birthdate',
+            'id'            => 'birthdate',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['gender'] = [
+            'name'          => 'gender',
+            'id'            => 'gender',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['gender_value'] = [
+            '0'             => '- Pilih Jenis Kelamin -',
+            '1'             => 'Laki-laki',
+            '2'             => 'Perempuan',
+        ];
+        $this->data['agama'] = [
+            'name'          => 'agama',
+            'id'            => 'agama',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['address'] = [
+            'name'          => 'address',
+            'id'            => 'address',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'rows'          => '2',
+            'required'      => '',
+        ];
+        $this->data['usaha_name'] = [
+            'name'          => 'usaha_name',
+            'id'            => 'usaha_name',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['address_usaha'] = [
+            'name'          => 'address_usaha',
+            'id'            => 'address_usaha',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+
+        $this->load->view('back/sk_usaha/sk_usaha_add', $this->data);
+    }
+
+    function create_action()
+    {
+        //TODO sistem validasi data inputan
+        $this->form_validation->set_rules('name', 'Nama', 'trim|required');
+        $this->form_validation->set_rules('nik', 'NIK', 'trim|required');
+        $this->form_validation->set_rules('birthplace', 'Tempat Lahir', 'trim|required');
+        $this->form_validation->set_rules('birthdate', 'Tanggal Lahir', 'required');
+        $this->form_validation->set_rules('gender', 'Jenis Kelamin', 'required');
+        $this->form_validation->set_rules('agama', 'Agama', 'required');
+        $this->form_validation->set_rules('address', 'Alamat', 'required');
+        $this->form_validation->set_rules('usaha_name', 'Nama Usaha', 'required');
+        $this->form_validation->set_rules('address_usaha', 'Alamat Tempat Usaha', 'required');
+
+        $this->form_validation->set_message('required', '{field} wajib diisi');
+
+        $this->form_validation->set_error_delimiters('<div class="alert alert-danger">', '</div>');
+
+        //?Apakah validasi gagal?
+        if ($this->form_validation->run() === FALSE) {
+            //TODO Kondisi validasi gagal, redirect ke halaman create
+            $this->create();
+        } else {
+            //TODO Simpan data ke array
+            $data = array(
+                'name'                  => $this->input->post('name'),
+                'nik'                   => $this->input->post('nik'),
+                'birthplace'            => $this->input->post('birthplace'),
+                'birthdate'             => $this->input->post('birthdate'),
+                'gender'                => $this->input->post('gender'),
+                'agama_id'              => $this->input->post('agama'),
+                'address'               => $this->input->post('address'),
+                'usaha_name'            => $this->input->post('usaha_name'),
+                'address_usaha'         => $this->input->post('address_usaha'),
+                'created_by'            => $this->session->username,
+            );
+
+            //TODO Post to database with model
+            $this->Sk_usaha_model->insert($data);
+
+            write_log();
+
+            //TODO Tampilkan notifikasi dan redirect
+            $this->session->set_flashdata('message', 'Sukses');
+            redirect('admin/sk_usaha');
+        }
+    }
 }
--- application/views/front/surat/create_sk_usaha.php
+++ application/views/front/surat/create_sk_usaha.php
@@ -20,7 +20,7 @@
                         <div class="col-lg-12">
                             <div class="box box-primary box-solid">
                                 <div class="box-header">
-                                    Form Pembuatan Surat Keterangan Usaha
+                                    <h3 class="box-title">Form Pembuatan Surat Keterangan Usaha</h3>
                                 </div>
                                 <div class="box-body">
                                     <div class="row">
