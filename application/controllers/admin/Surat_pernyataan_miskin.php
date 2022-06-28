cb9fd9328471a9f7a1e5b2ce1949fcf4ac5e1d6d
ridargustia <ridargp@gmail.com>
2022-06-28 09:50:10 +0700

    Membuat fitur tambah data surat pernyataan miskin pada sistem admin (back)

--- application/controllers/admin/Surat_pernyataan_miskin.php
+++ application/controllers/admin/Surat_pernyataan_miskin.php
@@ -40,4 +40,128 @@ class Surat_pernyataan_miskin extends CI_Controller
 
         $this->load->view('back/surat_pernyataan_miskin/surat_pernyataan_miskin_list', $this->data);
     }
+
+    function create()
+    {
+        //TODO Inisialisasi variabel
+        $this->data['page_title'] = $this->data['module'];
+        $this->data['action']     = 'admin/surat_pernyataan_miskin/create_action';
+
+        //TODO Get data untuk dropdown reference
+        $this->data['get_all_combobox_agama'] = $this->Agama_model->get_all_combobox();
+
+        //TODO Rancangan form
+        $this->data['name'] = [
+            'name'          => 'name',
+            'id'            => 'name',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['nik'] = [
+            'name'          => 'nik',
+            'id'            => 'nik',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthplace'] = [
+            'name'          => 'birthplace',
+            'id'            => 'birthplace',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['birthdate'] = [
+            'name'          => 'birthdate',
+            'id'            => 'birthdate',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
+        $this->data['gender'] = [
+            'name'          => 'gender',
+            'id'            => 'gender',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['gender_value'] = [
+            '0'             => '- Pilih Jenis Kelamin -',
+            '1'             => 'Laki-laki',
+            '2'             => 'Perempuan',
+        ];
+        $this->data['kebangsaan'] = [
+            'name'          => 'kebangsaan',
+            'id'            => 'kebangsaan',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['kebangsaan_value'] = [
+            '0'             => '- Pilih Kebangsaan -',
+            '1'             => 'Warga Negara Indonesia',
+            '2'             => 'Warga Negara Asing',
+        ];
+        $this->data['agama'] = [
+            'name'          => 'agama',
+            'id'            => 'agama',
+            'class'         => 'form-control',
+            'required'      => '',
+        ];
+        $this->data['address'] = [
+            'name'          => 'address',
+            'id'            => 'address',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'rows'          => '2',
+            'required'      => '',
+        ];
+
+        //TODO Load view dengan mengirim data
+        $this->load->view('back/surat_pernyataan_miskin/surat_pernyataan_miskin_add', $this->data);
+    }
+
+    function create_action()
+    {
+        //TODO sistem validasi data inputan
+        $this->form_validation->set_rules('name', 'Nama', 'trim|required');
+        $this->form_validation->set_rules('nik', 'NIK', 'trim|required');
+        $this->form_validation->set_rules('birthplace', 'Tempat Lahir', 'trim|required');
+        $this->form_validation->set_rules('birthdate', 'Tanggal Lahir', 'required');
+        $this->form_validation->set_rules('gender', 'Jenis Kelamin', 'required');
+        $this->form_validation->set_rules('kebangsaan', 'Kebangsaan', 'required');
+        $this->form_validation->set_rules('agama', 'Agama', 'required');
+        $this->form_validation->set_rules('address', 'Alamat', 'required');
+
+        $this->form_validation->set_message('required', '{field} wajib diisi');
+
+        $this->form_validation->set_error_delimiters('<div class="alert alert-danger">', '</div>');
+
+        //?Apakah validasi gagal?
+        if ($this->form_validation->run() === FALSE) {
+            //TODO Kondisi validasi gagal, redirect ke halaman create
+            $this->create();
+        } else {
+            //TODO Simpan data ke array
+            $data = array(
+                'name'                  => $this->input->post('name'),
+                'nik'                   => $this->input->post('nik'),
+                'birthplace'            => $this->input->post('birthplace'),
+                'birthdate'             => $this->input->post('birthdate'),
+                'gender'                => $this->input->post('gender'),
+                'kebangsaan'            => $this->input->post('kebangsaan'),
+                'agama_id'              => $this->input->post('agama'),
+                'address'               => $this->input->post('address'),
+                'created_by'            => $this->session->username,
+            );
+
+            //TODO Post to database with model
+            $this->Surat_pernyataan_miskin_model->insert($data);
+
+            write_log();
+
+            //TODO Tampilkan notifikasi dan redirect
+            $this->session->set_flashdata('message', 'Sukses');
+            redirect('admin/surat_pernyataan_miskin');
+        }
+    }
 }
