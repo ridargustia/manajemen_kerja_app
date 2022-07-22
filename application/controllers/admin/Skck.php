05be2591001f778414373c45bd7b8cc0b3ae57e4
ridargustia <ridargp@gmail.com>
2022-07-22 13:30:35 +0700

    Menambah input kolom no hp/telepon pada form tambah data skck di halaman admin

--- application/controllers/admin/Skck.php
+++ application/controllers/admin/Skck.php
@@ -88,6 +88,13 @@ class Skck extends CI_Controller
             'autocomplete'  => 'off',
             'required'      => '',
         ];
+        $this->data['phone'] = [
+            'name'          => 'phone',
+            'id'            => 'phone',
+            'class'         => 'form-control',
+            'autocomplete'  => 'off',
+            'required'      => '',
+        ];
         $this->data['gender'] = [
             'name'          => 'gender',
             'id'            => 'gender',
@@ -143,6 +150,7 @@ class Skck extends CI_Controller
         $this->form_validation->set_rules('nik', 'NIK', 'trim|required');
         $this->form_validation->set_rules('birthplace', 'Tempat Lahir', 'trim|required');
         $this->form_validation->set_rules('birthdate', 'Tanggal Lahir', 'required');
+        $this->form_validation->set_rules('phone', 'No HP/Telepon', 'required|is_numeric');
         $this->form_validation->set_rules('gender', 'Jenis Kelamin', 'required');
         $this->form_validation->set_rules('status', 'Status Pernikahan', 'required');
         $this->form_validation->set_rules('agama', 'Agama', 'required');
@@ -151,6 +159,7 @@ class Skck extends CI_Controller
         $this->form_validation->set_rules('address', 'Alamat', 'required');
 
         $this->form_validation->set_message('required', '{field} wajib diisi');
+        $this->form_validation->set_message('is_numeric', '{field} harus angka');
 
         $this->form_validation->set_error_delimiters('<div class="alert alert-danger">', '</div>');
 
@@ -165,6 +174,7 @@ class Skck extends CI_Controller
                 'nik'                   => $this->input->post('nik'),
                 'birthplace'            => $this->input->post('birthplace'),
                 'birthdate'             => $this->input->post('birthdate'),
+                'phone'                 => $this->input->post('phone'),
                 'gender'                => $this->input->post('gender'),
                 'status_id'             => $this->input->post('status'),
                 'agama_id'              => $this->input->post('agama'),
