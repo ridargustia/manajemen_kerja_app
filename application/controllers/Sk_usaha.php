40e2fc454892b274ed04af9c222c54311dd81f31
ridargustia <ridargp@gmail.com>
2022-10-25 21:29:39 +0700

    Membuat fitur authentikasi dokumen dengan pemohon menggunakan token dan tampilan detail data sk usaha dari antarmuka pemohon

--- application/controllers/Sk_usaha.php
+++ application/controllers/Sk_usaha.php
@@ -199,6 +199,55 @@ class Sk_usaha extends CI_Controller
         }
     }
 
+    function auth_download()
+    {
+        //TODO Inisialisasi variabel
+        $this->data['page_title'] = 'Authentikasi Download Dokumen';
+        $this->data['action']     = 'sk_usaha/auth_download_action';
+
+        //TODO Kondisi menampilkan halaman Auth download dokumen
+        $this->data['token'] = [
+            'name'              => 'token',
+            'id'                => 'token',
+            'class'             => 'form-control',
+            'autocomplete'      => 'off',
+            'required'          => '',
+            'value'             => $this->form_validation->set_value('token'),
+        ];
+
+        //TODO Load view halaman login
+        $this->load->view('front/surat/auth_download_sk_usaha', $this->data);
+    }
+
+    function auth_download_action()
+    {
+        //TODO sistem validasi data inputan
+        $this->form_validation->set_rules('token', 'Token', 'trim|required');
+
+        $this->form_validation->set_message('required', '{field} wajib diisi');
+
+        $this->form_validation->set_error_delimiters('<div class="alert alert-danger">', '</div>');
+
+        //?Apakah validasi gagal?
+        if ($this->form_validation->run() === FALSE) {
+            //TODO Kondisi validasi gagal, redirect ke halaman create
+            redirect('sk_usaha/auth_download');
+        } else {
+            $this->data['sk_usaha'] = $this->Sk_usaha_model->get_by_token($this->input->post('token'));
+
+            if ($this->data['sk_usaha']) {
+                $this->data['page_title'] = 'Download ' . $this->data['module'];
+                $this->data['agama'] = $this->Agama_model->get_by_id($this->data['sk_usaha']->agama_id);
+
+                $this->load->view('front/surat/download_sk_usaha', $this->data);
+            } else {
+                //TODO Tampilkan notifikasi dan redirect
+                $this->session->set_flashdata('message', '<div class="alert alert-danger">Akses gagal, silahkan hubungi Admin.</div>');
+                redirect('sk_usaha/auth_download');
+            }
+        }
+    }
+
     function check_format_phone()
     {
         $phone = $this->input->post('phone');
---
+++ application/views/front/surat/download_sk_usaha.php
@@ -0,0 +1,131 @@
+<?php $this->load->view('front/template/meta'); ?>
+
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+
+<body class="hold-transition skin-blue layout-top-nav">
+    <div class="wrapper">
+
+        <div class="content-wrapper bg">
+            <div class="container">
+                <!-- Content Header (Page header) -->
+                <section class="content-header"></section>
+
+                <!-- Main content -->
+                <section class="content">
+                    <div class="flash-data" data-flashdata="<?php echo $this->session->flashdata('message') ?>"></div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="box box-success">
+                                <div class="box-body">
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <?php if (empty($sk_usaha->signature_image)) { ?>
+                                                <img src="<?php echo base_url('assets/images/file_icon.png') ?>" width="100%" style="opacity: 0.5;">
+                                            <?php } else { ?>
+                                                <img src="<?php echo base_url('assets/images/file_icon_approved.png') ?>" width="100%">
+                                            <?php } ?>
+                                        </div>
+                                        <div class="col-md-9">
+                                            <table class="table">
+                                                <tbody>
+                                                    <tr>
+                                                        <td>No Surat</td>
+                                                        <td style="width:10px">:</td>
+                                                        <td class="text-left"><?php echo $sk_usaha->no_surat ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Nama</td>
+                                                        <td style="width:10px">:</td>
+                                                        <td class="text-left"><?php echo $sk_usaha->name ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>NIK</td>
+                                                        <td>:</td>
+                                                        <td class="text-left"><?php echo $sk_usaha->nik ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style="width:120px">Tempat Lahir</td>
+                                                        <td>:</td>
+                                                        <td class="text-left"><?php echo $sk_usaha->birthplace ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Tanggal Lahir</td>
+                                                        <td>:</td>
+                                                        <td class="text-left"><?php echo date_indonesian_only($sk_usaha->birthdate) ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td style="width:120px">No HP/Telepon</td>
+                                                        <td>:</td>
+                                                        <td class="text-left">+<?php echo $sk_usaha->phone ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Jenis Kelamin</td>
+                                                        <td>:</td>
+                                                        <td class="text-left">
+                                                            <?php
+                                                            if ($sk_usaha->gender === '1') {
+                                                                $gender = 'Laki-laki';
+                                                            } elseif ($sk_usaha->gender === '2') {
+                                                                $gender = 'Perempuan';
+                                                            }
+                                                            echo $gender;
+                                                            ?>
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Agama</td>
+                                                        <td>:</td>
+                                                        <td class="text-left"><?php echo $agama->agama_name ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Alamat</td>
+                                                        <td>:</td>
+                                                        <td class="text-left"><?php echo $sk_usaha->address ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Nama Usaha</td>
+                                                        <td>:</td>
+                                                        <td class="text-left"><?php echo $sk_usaha->usaha_name ?></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Alamat Tempat Usaha</td>
+                                                        <td>:</td>
+                                                        <td class="text-left"><?php echo $sk_usaha->address_usaha ?></td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box-footer">
+                                    <div class="pull-right">
+                                        <a href="<?php echo base_url('sk_usaha/preview_document/' . $sk_usaha->id_sk_usaha) ?>" class="btn btn-success" target="_blank"><i class="fa fa-download"></i> Download Surat</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box -->
+                </section>
+                <!-- /.content -->
+            </div>
+            <!-- /.container -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <?php $this->load->view('front/template/footer'); ?>
+
+        <!-- DataTables -->
+        <link rel="stylesheet" href="<?php echo base_url('assets/plugins/') ?>datatables-bs/css/dataTables.bootstrap.min.css">
+        <script src="<?php echo base_url('assets/plugins/') ?>datatables/js/jquery.dataTables.min.js"></script>
+        <script src="<?php echo base_url('assets/plugins/') ?>datatables-bs/js/dataTables.bootstrap.min.js"></script>
+
+        <script>
+            $(document).ready(function() {
+                $('#dataTable').DataTable();
+            });
+        </script>
+
+</body>
+
+</html>
