ae3ccfd5c37d42b92aecdd4b722293fe7b8dde00
ridargustia <ridargp@gmail.com>
2022-10-28 23:30:07 +0700

    Membuat fitur preview dan download dokumen surat rekomendasi dari antarmuka pemohon

--- application/controllers/Surat_rekomendasi.php
+++ application/controllers/Surat_rekomendasi.php
@@ -266,6 +266,135 @@ class Surat_rekomendasi extends CI_Controller
         }
     }
 
+    function preview_document($id_surat_rekomendasi)
+    {
+        $row = $this->Surat_rekomendasi_model->get_by_id_for_document($id_surat_rekomendasi);
+        $data_master = $this->Auth_model->get_by_usertype_master();
+
+        if ($row->gender === '1') {
+            $gender = 'Laki-laki';
+        } elseif ($row->gender === '2') {
+            $gender = 'Perempuan';
+        }
+
+        if ($row->kebangsaan === '1') {
+            $kebangsaan = 'WNI';
+        } elseif ($row->kebangsaan === '2') {
+            $kebangsaan = 'WNA';
+        }
+
+        require FCPATH . '/vendor/autoload.php';
+        require FCPATH . '/vendor/setasign/fpdf/fpdf.php';
+
+        $image = 'assets\images\kop_surat.png';
+        $ttd_kades = $row->signature_image;
+        $stempel = 'assets/images/stempel.png';
+
+        $pdf = new FPDF('P', 'mm', 'A4');
+        $pdf->SetTitle($this->data['module'] . ' a.n ' . $row->name);
+        $pdf->SetTopMargin(10);
+        $pdf->SetLeftMargin(25);
+        $pdf->SetRightMargin(25);
+        $pdf->AddFont('Calibri', '', 'calibri.php');
+        $pdf->AddFont('Calibrib', '', 'calibrib.php');
+        $pdf->AddPage();
+
+        //TODO Image
+        $pdf->Image($image, 25, 10, 24, 25);
+
+        //TODO Judul Surat
+        $pdf->SetFont('Arial', '', '11');
+        $pdf->Cell(0, 6, 'PEMERINTAH KABUPATEN SUMENEP', 0, 1, 'C');
+        $pdf->Cell(0, 6, 'KECAMATAN KANGAYAN', 0, 1, 'C');
+        $pdf->SetFont('Arial', 'B', '11');
+        $pdf->Cell(0, 6, 'KANTOR KEPALA DESA SAOBI', 0, 1, 'C');
+        $pdf->SetFont('Arial', '', '11');
+        $pdf->Cell(0, 6, 'Jalan Raya Masjid No. 50. Email desasaobi90@gmail.com', 0, 1, 'C');
+        $pdf->SetFont('Arial', 'BU', '11');
+        $pdf->Cell(0, 6, 'S A O B I', 0, 1, 'C');
+        $pdf->Cell(130);
+        $pdf->SetFont('Arial', 'I', '8');
+        $pdf->Cell(0, 3, 'Kode Pos 69491', 0, 1, 'C');
+
+        //TODO Body Surat
+        $pdf->SetFont('Calibrib', '', '12');
+        $pdf->Cell(0, 7, 'SURAT REKOMENDASI', 0, 1, 'C');
+        $pdf->SetFont('Calibri', '', '12');
+        $pdf->Cell(0, 5, 'Nomor : ' . $row->no_surat, 0, 1, 'C');
+
+        //TODO make a dummy empty cell as a vertical spacer
+        $pdf->Cell(0, 6, '', 0, 1); //end of line
+
+        //TODO Body Content
+        $pdf->MultiCell(0, 8, '     Yang bertanda tangan di bawah ini Kepala Desa Saobi Kecamatan Kangayan Kabupaten Sumenep, Menerangkan dengan Sebenarnya bahwa :', 0, 'J');
+        $pdf->Cell(50, 8, 'Nama', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->SetFont('Calibrib', '', '12');
+        $pdf->Cell(0, 8, strtoupper($row->name), 0, 1, 'L');
+        $pdf->SetFont('Calibri', '', '12');
+        $pdf->Cell(50, 8, 'Tempat / Tanggal Lahir', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->Cell(0, 8, $row->birthplace . ', ' . datetime_indo4($row->birthdate), 0, 1, 'L');
+        $pdf->Cell(50, 8, 'NIK', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->Cell(0, 8, $row->nik, 0, 1, 'L');
+        $pdf->Cell(50, 8, 'Jenis kelamin', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->Cell(0, 8, $gender, 0, 1, 'L');
+        $pdf->Cell(50, 8, 'Status', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->Cell(0, 8, $row->pekerjaan_name, 0, 1, 'L');
+        $pdf->Cell(50, 8, 'Agama', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->Cell(0, 8, $row->agama_name, 0, 1, 'L');
+        $pdf->Cell(50, 8, 'Kebangsaan', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->Cell(0, 8, $kebangsaan, 0, 1, 'L');
+        $pdf->Cell(50, 8, 'Pekerjaan', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->Cell(0, 8, $row->pekerjaan_name, 0, 1, 'L');
+        $pdf->Cell(50, 8, 'Alamat', 0, 0, 'L');
+        $pdf->Cell(5, 8, ' : ', 0, 0, 'C');
+        $pdf->Cell(0, 8, $row->address, 0, 1, 'L');
+        $pdf->Cell(50, 8, '', 0, 0, 'L');
+        $pdf->Cell(5, 8, '', 0, 0, 'C');
+        $pdf->Cell(0, 8, 'Saobi Kangayan Sumenep', 0, 1, 'L');
+
+        $pdf->MultiCell(0, 8, '     Bahwa yang bersangkutan sampai saat ini masih tercatat sebagai penduduk Desa Saobi kecamatan Kangayan Kabupaten Sumenep dan kami memberikan dukungan rekomendasi untuk melanjutkan pendidikan yang lebih tinggi di ' . $row->perguruan_tinggi . ' ' . $row->address_pt, 0, 'J');
+
+        $pdf->MultiCell(0, 8, 'Demikian surat rekomendasi ini dibuat untuk dipergunakan sebagaimana mestinya.', 0, 'J');
+
+        //TODO make a dummy empty cell as a vertical spacer
+        $pdf->Cell(0, 10, '', 0, 1); //end of line
+
+        $pdf->Cell(115);
+        $pdf->SetFont('Arial', 'I', '12');
+        if ($row->acc_at !== NULL) {
+            $pdf->Cell(0, 8, 'Saobi, ' . date_indonesian_only($row->acc_at), 0, 1, 'L');
+        } else {
+            $pdf->Cell(0, 8, 'Saobi, ', 0, 1, 'L');
+        }
+
+        $pdf->Cell(115);
+        $pdf->SetFont('Arial', '', '12');
+        $pdf->Cell(0, 8, 'Kepala Desa Saobi', 0, 1, 'C');
+
+        //TODO make a dummy empty cell as a vertical spacer
+        $pdf->Cell(0, 8, '', 0, 1); //end of line
+
+        if (!empty($row->signature_image)) {
+            //TODO Image
+            $pdf->Cell(50, 6, $pdf->Image($stempel, $pdf->GetX() + 100, $pdf->GetY() - 15, 35, 35), 0, 1, 'C');
+            $pdf->Cell(50, 6, $pdf->Image($ttd_kades, $pdf->GetX() + 120, $pdf->GetY() - 17, 35, 25), 0, 1, 'C');
+        }
+
+        $pdf->SetFont('Arial', 'BU', '12');
+        $pdf->Cell(107);
+        $pdf->Cell(60, 8, strtoupper($data_master->name), 0, 1, 'C');
+
+        $pdf->Output('I', $this->data['module'] . ' a.n ' . $row->name . '.pdf');
+    }
+
     function check_format_phone()
     {
         $phone = $this->input->post('phone');
